28488f675d797f99abcf69906ce4ade110727d5c
Yijun Jeon <Yijun@Yijunui-MacBookAir.local>
2022-12-26 22:22:21 +0900

    Make initial project draft

---
+++ Server(.NET_CORE)/ServerCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
