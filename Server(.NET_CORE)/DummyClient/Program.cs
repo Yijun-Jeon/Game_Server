fd5eb32fbbfb135bec53093ed5635b080fb1c8e0
Yijun Jeon <gkqls813@gmail.com>
2022-12-27 23:00:01 +0900

    Implement simple socket communication

--- Server(.NET_CORE)/DummyClient/Program.cs
+++ Server(.NET_CORE)/DummyClient/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Linq;
 
 namespace DummyClient
 {
@@ -6,7 +10,44 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DNS(Domain Name System)
+            string host = Dns.GetHostName();
+            // 로컬 컴퓨터의 host 이름
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            // 경우에 따라 주소 여러개의 배열을 반환함
+            IPAddress ipAddr = ipHost.AddressList[0];
+            // 최종 주소 - IP : 식당 주소  Port : 식당 문 번호
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 7000);
+
+            // 휴대폰 설정
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기에게 입장 문의
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World");
+                int sendBytes = socket.Send(sendBuff);
+
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            
         }
     }
 }
--- Server(.NET_CORE)/ServerCore/Program.cs
+++ Server(.NET_CORE)/ServerCore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
@@ -6,7 +9,56 @@ namespace ServerCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            // 로컬 컴퓨터의 host 이름
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            // 경우에 따라 주소 여러개의 배열을 반환함
+            IPAddress ipAddr = ipHost.AddressList[0];
+            // 최종 주소 - IP : 식당 주소  Port : 식당 문 번호
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 7000);
+
+            // www.yijun.com -> 123.123.123.12
+            // 문지기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp); // TCP 사용
+
+            try
+            {
+                // 문지기 교육
+                listenSocket.Bind(endPoint);
+
+                // 영업 시작
+                // backlog : 최대 대기 수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 손님을 입장시킴 - 대리인 생성
+                    // 입장을 하지 않으면 아래 단계는 진행이 되지 않음 
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    // 버퍼, 시작 인덱스, 받은 바이트
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫒아낸다
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
