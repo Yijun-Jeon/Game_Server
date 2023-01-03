b025038249822a1d3cd9aae6edcdb6242a15107f
Yijun Jeon <gkqls813@gmail.com>
2023-01-03 22:32:56 +0900

    Functionalize Packet Serialization & Deserialization

--- Server(.NET_CORE)/DummyClient/ServerSession.cs
+++ Server(.NET_CORE)/DummyClient/ServerSession.cs
@@ -2,24 +2,67 @@
 using ServerCore;
 using System.Net;
 using System.Text;
+using System.Drawing;
 
 namespace DummyClient
 {
-    class Packet
+    public abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
+
     }
     // 플레이어 정보 요청 패킷  
     class PlayerInfoReq : Packet
     {
         public long playerId;
-    }
-    // 플레이어 정보 응답 패킷
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
+
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+
+            // 패킷에서 정보 추출 
+            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            //ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+
+            // 범위 초과 값은 자동으로 에러 처리 
+            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // 공간이 모자르면 실패
+            // Count -> 쓸 수 있는 공간
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+
+            // 패킷의 size는 모든 작업이 끝난 후 넣어줘야 제대로 측정이 가능
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+            if (success == false)
+                return null;
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
     // 패킷의 분류를 위한 ID
@@ -35,31 +78,14 @@ namespace DummyClient
         {
             Console.WriteLine($"OnConnected: {endPoint}");
 
-            PlayerInfoReq packet = new PlayerInfoReq() { packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
 
             // 보낸다
             //for (int i = 0; i < 5; i++)
             {
-                ArraySegment<byte> s = SendBufferHelper.Open(4096);
-
-                ushort count = 0;
-                bool success = true;
-
-                // 공간이 모자르면 실패
-                // Count -> 쓸 수 있는 공간
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
-                count += 8;
-
-                // 패킷의 size는 모든 작업이 끝난 후 넣어줘야 제대로 측정이 가능
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
-
-
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
-
-                Send(sendBuff);
+                ArraySegment<byte> s = packet.Write();
+                if(s != null)
+                    Send(s);
             }
         }
 
--- Server(.NET_CORE)/Server/ClientSession.cs
+++ Server(.NET_CORE)/Server/ClientSession.cs
@@ -5,21 +5,63 @@ using System.Threading;
 
 namespace Server
 {
-    class Packet
+    public abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
+
     }
     // 플레이어 정보 요청 패킷  
     class PlayerInfoReq : Packet
     {
         public long playerId;
-    }
-    // 플레이어 정보 응답 패킷
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
+
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+
+            // 패킷에서 정보 추출 
+            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            //ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+
+            // 범위 초과 값은 자동으로 에러 처리 
+            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // 공간이 모자르면 실패
+            // Count -> 쓸 수 있는 공간
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+
+            // 패킷의 size는 모든 작업이 끝난 후 넣어줘야 제대로 측정이 가능
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+            if (success == false)
+                return null;
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
     // 패킷의 분류를 위한 ID
@@ -48,7 +90,7 @@ namespace Server
             //Send(sendBuff);
 
             Thread.Sleep(100);
-            Disconnect();
+            //Disconnect();
         }
 
         public override void OnRecvPacket(ArraySegment<byte> buffer)
@@ -66,9 +108,9 @@ namespace Server
             {
                 case PacketID.PlayerInfoReq:
                     {
-                        long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
-                        count += 8;
-                        Console.WriteLine($"PlayerInfoReq: {playerId}");
+                        PlayerInfoReq p = new PlayerInfoReq();
+                        p.Read(buffer);
+                        Console.WriteLine($"PlayerInfoReq: {p.playerId}");
                     }
                     break;
             }
